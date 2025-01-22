0818d8ed67b183103b9c20128f3cf6eb9ad8b131
gluoneros <gluoneros@gmail.com>
2025-01-22 11:20:17 -0500

    register

--- users/views.py
+++ users/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
+def login_view(request):
+    return render(request, 'users/login.html')
 
+def register_view(request):
+    return render(request, 'users/register.html')
