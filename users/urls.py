11458942e2708433947ffee7afe6a55fab5e7472
gluoneros <gluoneros@gmail.com>
2025-06-29 21:41:06 -0500

    login y registro

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,15 @@
+from django import contrib
+from django.contrib.auth import login
 from django.urls import path
 from .views import registro_usuario_general
 from django.shortcuts import render
+from django.contrib.auth import views as auth_views
 
 #from users.views import CustomLoginView
 #from users.views import RegisterView
 
 urlpatterns = [
+    path('login/',  contrib.auth.views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('registro/', registro_usuario_general, name='registro'),
     path('registro-exitoso/', lambda request: render(request, 'users/registro_exitoso.html'), name='registro_exitoso')
 ]
