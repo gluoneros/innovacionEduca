5df79a30ca9ca6918a7239b464c856ee9b51e401
gluoneros <gluoneros@gmail.com>
2025-09-12 17:58:42 -0500

    refactor: update student dashboard layout and content sections, perfil estudiante

--- notas/views.py
+++ notas/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy, reverse
+from django.contrib import messages
+from django.db import transaction
+from django.http import JsonResponse
+from django.core.exceptions import ValidationError
 
-# Create your views here.
+from .models import AnioEscolar, Grado, Materia, Periodo, Nota, InformeFinal, EscalaNota
+from users.models import Estudiante, Profesor
+
+
+class AnioEscolarListView(ListView):
+    model = AnioEscolar
+    template_name = 'notas/anio_list.html'
+    context_object_name = 'anios'
