df34b6b7e54f0f3e68f57a29114d8897efe94003
gluoneros <gluoneros@gmail.com>
2025-09-12 23:31:54 -0500

    feat: add AnioEscolarDetailView and PeriodoCreateView with context data and form validation for creating periods

--- notas/views.py
+++ notas/views.py
@@ -14,3 +14,35 @@ class AnioEscolarListView(ListView):
     model = AnioEscolar
     template_name = 'notas/anio_list.html'
     context_object_name = 'anios'
+
+class AnioEscolarDetailView(DetailView):
+    model = AnioEscolar
+    template_name = 'notas/anio_detail.html'
+    context_object_name = 'anio'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['periodos'] = self.object.periodos.all()
+        context['grados'] = self.object.grados.all()
+        return context
+    
+class PeriodoCreateView(CreateView):
+    model = Periodo
+    fields = ['nombre', 'porcentaje']
+    template_name = 'notas/periodo_form.html'
+    
+    def form_valid(self, form):
+        anio = get_object_or_404(AnioEscolar, pk=self.kwargs['anio_id'])
+        form.instance.anio_escolar = anio
+        try:
+            form.instance.clean()
+        except ValidationError as e:
+            form.add_error(None, e)
+            return self.form_invalid(form)
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+        return reverse_lazy('anio_detail', kwargs={'pk': self.kwargs['anio_id']})
+    
+
+    
